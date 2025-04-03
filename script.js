1fc67c1ff2bec7f544b77bcaf371a05174df38d5
Luka Popovic <lukapopovic0111@gmail.com>
2025-04-03 15:11:23 +0200

    Creating variables

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-"use strict";
+'use strict';
+const guess = document.querySelector('.guess');
+const check = document.querySelector('.btn-check');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highscore = document.querySelector('.highscore');
